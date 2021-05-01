6fbd10ec9e11247a03e37d005f1193e79380967b
michaelCaleyWhaley <kneedeepwater@hotmail.com>
2021-05-02 00:59:26 +0100

    flex grow

--- render-react-server-side/src/SubMenu/SubMenu.js
+++ render-react-server-side/src/SubMenu/SubMenu.js
@@ -1,14 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./SubMenu.scss";
 
 const SubMenu = ({ SubMenuData }) => {
+  useState(() => {
+    setTimeout(() => {
+      document.querySelector(".nav__item").classList.add("nav__item--open");
+    }, 500);
+  }, []);
+
   return (
     <div className="sub-menu">
       {SubMenuData.map((menu) => {
         if (!menu) return null;
         return (
-          <ul key={menu[0].name + "ul"}>
+          <ul className="sub-menu__list" key={menu[0].name + "ul"}>
             {menu.map((column) => {
               return (
                 <li key={column.name + "li"}>
